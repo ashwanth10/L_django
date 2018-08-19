f64f10a8b230cb6174ad050914a63cb79df004a0
Aswanth <Aswanth.KumarM@microfocus.com>
2018-08-19 18:15:53 +0530

    added hello

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,6 @@
-from django.http import HTTPResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def posts_home(request):
-	return HTTPResponse("<h1>Hello</h1>")
+def post_home(request):
+	return HttpResponse("<h1>Hello</h1>")
