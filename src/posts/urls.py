10b414452d2e127ddccf8f112e186b5897a5390f
Aswanth <Aswanth.KumarM@microfocus.com>
2018-08-20 21:35:38 +0530

    url links and get absolute url

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 urlpatterns = [
 	url(r'^$', post_list),
 	url(r'^create/$', post_create),
-	url(r'^(?P<id>\d+)/$', post_detail),
+	url(r'^(?P<id>\d+)/$', post_detail, name="detail"),
 	url(r'^update/$', post_update),
 	url(r'^delete/$', post_delete),
 ]
