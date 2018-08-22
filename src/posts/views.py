23b337c6192b3ce1d9f4fd25efe6d968b0a239d3
Aswanth <Aswanth.KumarM@microfocus.com>
2018-08-22 18:23:17 +0530

    creating messages

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from .models import Post
@@ -12,8 +13,12 @@ def post_create(request):
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
+		messages.success(request, "created form successfully")
 		# success showing the post has been created
 		return HttpResponseRedirect(instance.get_absolute_url())
+	else:
+		messages.error(request, "couldn't create a message")
+		
 	context = {
 		"form" : form,
 	}
@@ -42,6 +47,7 @@ def post_update(request, id=None):
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
+		messages.success(request, "<a href='#'>form</a> updated", extra_tags='html_safe')
 		return HttpResponseRedirect(instance.get_absolute_url())
 
 	context = {
